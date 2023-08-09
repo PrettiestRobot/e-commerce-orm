9c9b3c57087bb8751fc504e6f434abe86686cfd3
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-08-09 17:10:29 -0400

    product routes updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -109,8 +109,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProductData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteProductData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+    res.status(200).json(deleteProductData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
